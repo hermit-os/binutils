1a6a527f91de542779f0d870120c36e0866d12c4
Stefan Lankes <slankes@eonerc.rwth-aachen.de>
2016-12-26 18:54:05 +0100

    set prefix to /opt/hermit

--- debian/emacsen-remove.ex
+++ debian/emacsen-remove.ex
@@ -7,7 +7,7 @@ PACKAGE=binutils
 if [ ${FLAVOR} != emacs ]; then
     if test -x /usr/sbin/install-info-altdir; then
         echo remove/${PACKAGE}: removing Info links for ${FLAVOR}
-        install-info-altdir --quiet --remove --dirname=${FLAVOR} /usr/share/info/binutils.info.gz
+        install-info-altdir --quiet --remove --dirname=${FLAVOR} /opt/hermit/share/info/binutils.info.gz
     fi
 
     echo remove/${PACKAGE}: purging byte-compiled files for ${FLAVOR}
