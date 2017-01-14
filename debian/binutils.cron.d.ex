17a925441db099597439964854a196bb8224101b
Daniel Krebs <github@daniel-krebs.net>
2017-01-14 23:09:01 +0100

    deb: delete unused files

--- debian/binutils.cron.d.ex
+++
@@ -1,4 +0,0 @@
-#
-# Regular cron jobs for the binutils package
-#
-0 4	* * *	root	[ -x /usr/bin/binutils_maintenance ] && /usr/bin/binutils_maintenance
--- debian/menu.ex
+++
@@ -1,2 +0,0 @@
-?package(binutils):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
-  title="binutils" command="/usr/bin/binutils"
