f5702e440e474745a3330f527fa74cd73f42939b
Stefan Lankes <slankes@eonerc.rwth-aachen.de>
2017-08-27 19:06:59 +0200

    use docker to build packages for different Linux distributions

--- tests.sh
+++ tests.sh
@@ -3,8 +3,6 @@
 OS_NAME=$1
 OS_VERSION=$2
 
-cd /rwth-os/binutils/
-
 if [ "$OS_NAME" = "centos" ]; then
 
 # Clean the yum cache
@@ -12,7 +10,7 @@ yum -y clean all
 yum -y clean expire-cache
 
 # First, install all the needed packages.
-yum install -y binutils gcc gcc-c++ texinfo kernel-headers rpm-build kernel-devel boost-devel cmake git tar gzip make autotools
+yum install -y flex bison binutils gcc gcc-c++ texinfo kernel-headers rpm-build kernel-devel boost-devel cmake git tar gzip make autotools
 
 # Prepare the RPM environment
 mkdir -p /tmp/rpmbuild/{BUILD,RPMS,SOURCES,SPECS,SRPMS}
@@ -23,10 +21,13 @@ EOF
 
 elif [ "$OS_NAME" = "ubuntu" ]; then
 
+export DEBIAN_FRONTEND="noninteractive"
+
 apt-get -qq update
-apt-get install -y binutils texinfo gcc g++ packaging-dev build-essential kernel-package
+apt-get install -y dialog apt-utils flex bison binutils texinfo gcc g++ packaging-dev build-essential libtool autotools-dev autoconf pkg-config
 
 time debuild -us -uc -j2
+echo $?
 md5sum ../*.deb
 
 fi
