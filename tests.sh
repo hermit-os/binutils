7286852aa0c92832903261f1ea524c142cbdafd2
Colin Finck <colin.finck@rwth-aachen.de>
2018-06-12 11:44:38 +0200

    Fix formatting in tests.sh and add an explanation for the ugly workaround.
    No code changes.

--- tests.sh
+++ tests.sh
@@ -5,44 +5,46 @@ OS_VERSION=$2
 
 if [ "$OS_NAME" = "centos" ]; then
 
-# Clean the yum cache
-yum -y clean all
-yum -y clean expire-cache
+	# Clean the yum cache
+	yum -y clean all
+	yum -y clean expire-cache
 
-# First, install all the needed packages.
-yum install -y wget gettext flex bison binutils gcc gcc-c++ texinfo kernel-headers rpm-build kernel-devel boost-devel cmake git tar gzip make autotools
+	# First, install all the needed packages.
+	yum install -y wget gettext flex bison binutils gcc gcc-c++ texinfo kernel-headers rpm-build kernel-devel boost-devel cmake git tar gzip make autotools
 
-wget http://checkinstall.izto.org/files/source/checkinstall-1.6.2.tar.gz
-tar xzvf checkinstall-1.6.2.tar.gz
-cd checkinstall-1.6.2
-./configure
-make
-make install
-cd ..
-rm -rf checkinstall*
+	wget http://checkinstall.izto.org/files/source/checkinstall-1.6.2.tar.gz
+	tar xzvf checkinstall-1.6.2.tar.gz
+	cd checkinstall-1.6.2
+	./configure
+	make
+	make install
+	cd ..
+	rm -rf checkinstall*
 
-mkdir -p build
-cd build
-../configure --target=x86_64-hermit --prefix=/opt/hermit --disable-shared --disable-nls --disable-gdb --disable-libdecnumber --disable-readline --disable-sim --disable-libssp --enable-tls --disable-multilib
-make
-checkinstall -R -y --exclude=build --pkggroup=main --maintainer=stefan@eonerc.rwth-aachen.de --pkgsource=https://hermitcore.org --pkgname=binutils-hermit --pkgversion=2.30.51 --pkglicense=GPL2 make install
+	mkdir -p build
+	cd build
+	../configure --target=x86_64-hermit --prefix=/opt/hermit --disable-shared --disable-nls --disable-gdb --disable-libdecnumber --disable-readline --disable-sim --disable-libssp --enable-tls --disable-multilib
+	make
+	checkinstall -R -y --exclude=build --pkggroup=main --maintainer=stefan@eonerc.rwth-aachen.de --pkgsource=https://hermitcore.org --pkgname=binutils-hermit --pkgversion=2.30.51 --pkglicense=GPL2 make install
 
 else
 
-export DEBIAN_FRONTEND="noninteractive"
+	export DEBIAN_FRONTEND="noninteractive"
 
-apt-get -qq update
-apt-get install -y checkinstall gawk dialog apt-utils flex bison binutils texinfo gcc g++ libmpfr-dev libmpc-dev libgmp-dev libisl-dev packaging-dev build-essential libtool autotools-dev autoconf pkg-config
+	apt-get -qq update
+	apt-get install -y checkinstall gawk dialog apt-utils flex bison binutils texinfo gcc g++ libmpfr-dev libmpc-dev libgmp-dev libisl-dev packaging-dev build-essential libtool autotools-dev autoconf pkg-config
 
-mkdir -p build
-cd build
-../configure --target=x86_64-hermit --prefix=/opt/hermit --disable-shared --disable-nls --disable-gdb --disable-libdecnumber --disable-readline --disable-sim --disable-libssp --enable-tls --disable-multilib
-make
-checkinstall -D -y --exclude=build --pkggroup=main --maintainer=stefan@eonerc.rwth-aachen.de --pkgsource=https://hermitcore.org --pkgname=binutils-hermit --pkgversion=2.30.51 --pkglicense=GPL2 make install
+	mkdir -p build
+	cd build
+	../configure --target=x86_64-hermit --prefix=/opt/hermit --disable-shared --disable-nls --disable-gdb --disable-libdecnumber --disable-readline --disable-sim --disable-libssp --enable-tls --disable-multilib
+	make
+	checkinstall -D -y --exclude=build --pkggroup=main --maintainer=stefan@eonerc.rwth-aachen.de --pkgsource=https://hermitcore.org --pkgname=binutils-hermit --pkgversion=2.30.51 --pkglicense=GPL2 make install
 
-cd ..
-mkdir -p tmp
-dpkg-deb -R build/binutils-hermit_2.30.51-1_amd64.deb tmp
-rm -f build/binutils-hermit_2.30.51-1_amd64.deb
+	# Unpack the created .deb and let Travis repackage it.
+	# This is a workaround for Bintray, which doesn't understand the default .deb compression of Ubuntu 18.04.
+	cd ..
+	mkdir -p tmp
+	dpkg-deb -R build/binutils-hermit_2.30.51-1_amd64.deb tmp
+	rm -f build/binutils-hermit_2.30.51-1_amd64.deb
 
 fi
